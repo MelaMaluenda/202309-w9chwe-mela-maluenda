ca26818fec8c5d711a06011011d1908406cc2945
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-11 17:27:30 +0100

    Intall testing library react

--- src/main.tsx
+++ src/main.tsx
@@ -8,11 +8,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import mainTheme from "./styles/mainTheme";
 import { ThemeProvider } from "styled-components";
+import App from "./components/App/App";
+import { BrowserRouter } from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThemeProvider theme={mainTheme}>
-      <div />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
