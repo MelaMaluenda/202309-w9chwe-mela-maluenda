ec724120a53576733876cb65b87f08306d718278
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-11 13:35:36 +0100

    Add main theme styles

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,28 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colours: {
+    offWhite: "#f3f3e3",
+    cream: "#f3f2e4",
+    darkBrown: "#32191f",
+    red: "#bd313e",
+    garnet: "#6f151f",
+    orange: "#f5aa66",
+    yellow: "#facb48",
+    lime: "#c7d259",
+    turquoise: "#61bfb5",
+  },
+
+  tpography: {
+    mainFamily: "'Slabo 27px', serif",
+    regularFamily: "'Nunito Sans', sans-serif;",
+    placeholderFamily: "'Raleway Dots', sans-serif",
+    mainSize: "3.5size",
+    titleSize: "3rem",
+    regularSize: "2rem",
+    formSize: "1rem",
+    smallSize: "0.6rem",
+  },
+};
+
+export default mainTheme;
