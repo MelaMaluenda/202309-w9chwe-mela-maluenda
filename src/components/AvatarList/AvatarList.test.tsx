41c4bfd413936af8fe23429d904be7b2315c8803
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-12 07:11:09 +0100

    Add AvatarList test component

---
+++ src/components/AvatarList/AvatarList.test.tsx
@@ -0,0 +1,32 @@
+import avatarMock from "../../mocks/avatarMock/avatarMock";
+import customRender from "../../testUtils/customRender";
+import AvatarList from "./AvatarList";
+import { screen } from "@testing-library/react";
+
+describe("Given the AvatarList component", () => {
+  beforeEach(() => {
+    customRender(<AvatarList />);
+  });
+
+  describe("When it received an AvatarCard", () => {
+    test("Then it should show a card with an image and it's alternative text `Icon element: Tierra`", () => {
+      const expextedAltElementText = avatarMock;
+
+      const avatarElement = screen.getAllByAltText(
+        `Icon element: ${expextedAltElementText.element}`,
+      );
+
+      expect(avatarElement).toBeInTheDocument;
+    });
+
+    test("Then it should show a card with the name: `Hermione Granger`", () => {
+      const expectedAudiovisual = avatarMock;
+
+      const avatarAudiovisual = screen.getByRole("heading", {
+        name: expectedAudiovisual.name,
+      });
+
+      expect(avatarAudiovisual).toBeInTheDocument();
+    });
+  });
+});
