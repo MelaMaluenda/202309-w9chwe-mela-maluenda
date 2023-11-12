55a47724550a5bfc82f25bd8e9abf144717d4349
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-12 23:57:40 +0100

    Add new page path route into app component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../../pages/HomePage/HomePage";
+import CreatePage from "../../pages/CreatePage/CreatePage";
 
 const App = (): React.ReactElement => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/aquelarre" />} />
       <Route path="/aquelarre" element={<HomePage />} />
+      <Route path="/iniciar-sesion" element={<CreatePage />} />
     </Routes>
   );
 };
