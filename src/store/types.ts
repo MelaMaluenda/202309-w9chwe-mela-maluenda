5a21d82afdf1b9c02c3bbacc382944ac0b88fb98
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-12 01:51:10 +0100

    Add avatar types

---
+++ src/store/types.ts
@@ -0,0 +1,13 @@
+interface AvatarStructure {
+  id: number;
+  name: string;
+  element: string;
+  characterTotem: string;
+  book: string;
+  audiovisual: string;
+  videoGame: string;
+  pictureUrl: string;
+  isAlly: string;
+}
+
+export default AvatarStructure;
