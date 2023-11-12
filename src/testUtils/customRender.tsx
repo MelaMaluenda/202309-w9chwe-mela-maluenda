4ce5b3b6dc2bbd190e06e4f300d2462faae5567e
Mela Maluenda <maluenda.mela@gmail.com>
2023-11-12 11:52:55 +0100

    Modify browser to memory router

--- src/testUtils/customRender.tsx
+++ src/testUtils/customRender.tsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
+import { MemoryRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import mainTheme from "../styles/mainTheme";
 import { render } from "@testing-library/react";
 
 const customRender = (children: React.ReactElement) =>
   render(
-    <BrowserRouter>
+    <MemoryRouter>
       <ThemeProvider theme={mainTheme}>{children}</ThemeProvider>
-    </BrowserRouter>,
+    </MemoryRouter>,
   );
 
 export default customRender;
